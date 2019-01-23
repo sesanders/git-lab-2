97b0d96b01100aa4e602b4f6f12a006d703caf3a
sesanders <ss528714@ohio.edu>
2019-01-22 19:04:41 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *         File: git-lab-program.cc
+ *       Author: Samantha Sanders
+ *        Date : 1.22.2019
+ *  Description: 
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+
+}
